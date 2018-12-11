812574d73108af403db40d438eabbbef2110250f
Larry Su <larry7696@gmail.com>
2018-12-12 12:12:43 +1300

    Create WechatSignInAdapter.java

---
+++ src/main/java/org/springframework/social/wechat/sample/WechatSignInAdapter.java
@@ -0,0 +1,22 @@
+package org.springframework.social.wechat.sample;
+
+import org.springframework.social.connect.Connection;
+import org.springframework.social.connect.web.SignInAdapter;
+import org.springframework.social.wechat.api.User;
+import org.springframework.social.wechat.api.Wechat;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.NativeWebRequest;
+
+@Component
+public class WechatSignInAdapter implements SignInAdapter {
+
+	@Override
+	public String signIn(String openId, Connection<?> connection, NativeWebRequest request) {
+		if (connection.getApi() instanceof Wechat) {
+			User user = ((Wechat) connection.getApi()).userOperations().getUserProfile(openId);
+			System.out.println(user); // 微信用户详细信息
+		}
+		return null; // 返回跳转的url
+	}
+
+}
