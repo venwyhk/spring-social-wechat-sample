e53f0c6ffbde08e35155d787f9583cd631dbe947
Larry Su <larry7696@gmail.com>
2018-12-12 12:33:24 +1300

    Update WechatSignInAdapter.java

--- src/main/java/org/springframework/social/wechat/sample/WechatSignInAdapter.java
+++ src/main/java/org/springframework/social/wechat/sample/WechatSignInAdapter.java
@@ -1,6 +1,7 @@
 package org.springframework.social.wechat.sample;
 
 import org.springframework.social.connect.Connection;
+import org.springframework.social.connect.ConnectionKey;
 import org.springframework.social.connect.web.SignInAdapter;
 import org.springframework.social.wechat.api.User;
 import org.springframework.social.wechat.api.Wechat;
@@ -12,7 +13,8 @@ public class WechatSignInAdapter implements SignInAdapter {
 
 	@Override
 	public String signIn(String openId, Connection<?> connection, NativeWebRequest request) {
-		if (connection.getApi() instanceof Wechat) {
+		ConnectionKey key = connection.getKey();
+		if ("wechat".equalsIgnoreCase(key.getProviderId()) || "wechatmp".equalsIgnoreCase(key.getProviderId())) {
 			User user = ((Wechat) connection.getApi()).userOperations().getUserProfile(openId);
 			System.out.println(user); // 微信用户详细信息
 		}
