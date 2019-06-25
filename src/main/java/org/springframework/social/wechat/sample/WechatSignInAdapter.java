77a0f9617c6ff50445e0bfb5eff9bf2003e8b4fd
Larry <larry7696@gmail.com>
2019-06-25 15:47:07 +0800

    add annotations

--- src/main/java/org/springframework/social/wechat/sample/WechatSignInAdapter.java
+++ src/main/java/org/springframework/social/wechat/sample/WechatSignInAdapter.java
@@ -14,9 +14,9 @@ public class WechatSignInAdapter implements SignInAdapter {
 	@Override
 	public String signIn(String openId, Connection<?> connection, NativeWebRequest request) {
 		ConnectionKey key = connection.getKey();
-		if ("wechat".equalsIgnoreCase(key.getProviderId()) || "wechatmp".equalsIgnoreCase(key.getProviderId())) {
-			User user = ((Wechat) connection.getApi()).userOperations().getUserProfile(openId);
-			System.out.println(user); // 微信用户详细信息
+		if ("wechat".equalsIgnoreCase(key.getProviderId()) || "wechatmp".equalsIgnoreCase(key.getProviderId())) { // 这里的if判断可对微信开放平台和微信公众平台帐号登录做不同的逻辑处理，此例子代码处理逻辑相同
+			User user = ((Wechat) connection.getApi()).userOperations().getUserProfile(openId); // 默认语言为英文，如果想切换为中文可改为:getUserProfile(openId,WechatLangEnum.ZH_CN);
+			System.out.println(user); // 打印微信用户详细信息
 		}
 		return "/login.htm"; // 返回跳转的url
 	}
